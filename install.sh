095c8673fdd6307ed5f5d8cd2957b8deb65ed778
LuisSuall <luissuall@gmail.com>
2023-11-21 20:50:39 +0100

    Add instal.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install tmux fzf neofetch stow
+
+# Starship
+curl -sS https://starship.rs/install.sh | sh
+
+# Nvim stable
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
