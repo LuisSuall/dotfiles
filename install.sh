77849498d279c01616ca0e36acdea51f3bf7adad
LuisSuall <luissuall@gmail.com>
2024-01-21 20:55:01 +0100

    Add lazygit and tmux shortcut

--- install.sh
+++ install.sh
@@ -14,6 +14,12 @@ sudo snap install alacritty --classic
 # TPM plugin manager installation for TMUX (<leader> + I to install plugins)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install Lazy git, check repo
+# LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+# curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+# tar xf lazygit.tar.gz lazygit
+# sudo install lazygit /usr/local/bin
+
 # Qtile
 sudo apt-get install python3 python3-pip
 pip install xcffib
