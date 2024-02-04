7f1208a4d5bff7a7ae5566689d3c2eca09bcd322
Luis Suárez <luissuall@gmail.com>
2024-02-04 18:42:58 +0100

    Adding some safety to forced stow

--- stow_all.sh
+++ stow_all.sh
@@ -1,3 +1,8 @@
+if [[ `git status --porcelain` ]]; then
+    git add .
+    git commit -m "tmp: commit before stow --adopt"
+fi
+
 stow --adopt bash nvim tmux alacritty qtile
 git reset --hard HEAD
 
